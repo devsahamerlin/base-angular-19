b1eb65acb57870e1b62e8ab750ee6d05a75b0e29
Merlin Saha <devsahamerlin@gmail.com>
2025-06-07 19:05:01 +0400

    feat(products): add products list structures

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css',
   standalone: true
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  products! : Array<any>
+  constructor() { }
+
+  ngOnInit(): void {
+    this.products = [
+      { id: 1, name: 'Computer', price: 2300, selected: true },
+      { id: 2, name: 'Printer', price: 1200, selected: false  },
+      { id: 3, name: 'Smart Phone', price: 1100, selected: false  }
+    ]
+  }
+
+  handleDelete(product: any) {
+    let validate = confirm(`Are you sure you want to delete ${product.name}?`);
+    if (validate == true) {
+      this.products = this.products.filter((p:any) => p.id != product.id);
+    }
+  }
 }
